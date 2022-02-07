0325fec52a19e70a2c6edd2e8b9f67c18cde2762
apaximperator <apaximperator@gmail.com>
2022-02-07 18:38:26 +0200

    фильтр и сортировка

--- tests/_support/CategoryTester.php
+++ tests/_support/CategoryTester.php
@@ -8,53 +8,6 @@ class CategoryTester extends GlobalTester
     /**
      * @throws Exception
      */
-    public function openRandomNotEmptyBrandCategory()
-    {
-        $C = $this;
-        $brandCategoryWithoutProducts = true;
-        while ($brandCategoryWithoutProducts) {
-            $C->moveMouseOver('//a[@id="ui-id-' . rand(2, 3) . '"]');
-            $C->waitForElementClickable('li.hover div.brands-logo a');
-            $C->wait(2);
-            $BrandCategoryCount = $C->getElementsCountByCssSelector('li.hover div.brands-logo a');
-            $BrandCategoryNumber = rand(1, $BrandCategoryCount + 1);
-            $BrandLink = $C->grabAttributeFrom('//*[contains(@class, "hover")]//div[@class="brands-item"][' . $BrandCategoryNumber . ']//a', 'href');
-            $BrandLink = str_replace(Credentials::$URL, '', $BrandLink);
-            $C->click('//*[contains(@class, "hover")]//div[@class="brands-item"][' . $BrandCategoryNumber . ']//a');
-            $C->waitPageLoad();
-            $C->seeInCurrentUrl($BrandLink);
-            try {
-                $C->seeElement('//div[@class="product actions product-item-actions"]/div/a');
-                $brandCategoryWithoutProducts = false;
-            } catch (Exception $e) {
-                $brandCategoryWithoutProducts = true;
-            }
-        }
-    }
-
-    /**
-     * @throws Exception
-     */
-    public function openRandomNotEmptyCLP()
-    {
-        $C = $this;
-        $C->connectJq();
-        $categoryWithoutProducts = true;
-        while ($categoryWithoutProducts) {
-            $C->click('//a[@id="ui-id-' . rand(2, 3) . '"]');
-            $C->waitPageLoad();
-            try {
-                $C->seeElement('//div[@class="product actions product-item-actions"]/div/a');
-                $categoryWithoutProducts = false;
-            } catch (Exception $e) {
-                $categoryWithoutProducts = true;
-            }
-        }
-    }
-
-    /**
-     * @throws Exception
-     */
     public function openRandomNotEmptyPLP()
     {
         $C = $this;
@@ -71,7 +24,7 @@ class CategoryTester extends GlobalTester
                 $C->waitForElementClickable('//*[contains(@class, "hover")]//div[@class="bottom-links"]//a[contains(text(), "All")]');
                 $C->click('//*[contains(@class, "hover")]//div[@class="bottom-links"]//a[contains(text(), "All")]');
                 $C->waitPageLoad();
-               if ($nav === 2){ //fix recommended tab
+                if ($nav === 2) { //fix recommended tab
                     $C->click('//div[@class="recommended-tab all-options"]');
                 }
             }
@@ -110,27 +63,21 @@ class CategoryTester extends GlobalTester
     public function selectRandomFilter()
     {
         $C = $this;
-        $C->connectJq();
-        $dropdownFiltersCount = $C->getElementsCountByCssSelector(".filter-options-title");
-        $randomDropdownFilter = rand(0, $dropdownFiltersCount - 1);
-        $C->executeJS("document.querySelectorAll(\".filter-options-title\")[$randomDropdownFilter].click()");
+        $C->waitPageLoad();
+        $C->clickOnElementByCssSelector('div.is-widget-container-brand div.name');
         $C->waitAjaxLoad();
-        $filterName = $C->executeJS("return document.querySelector(\"div[aria-selected='true']\").innerText;");
-        if ($filterName == 'COLOR') {
-            $C->waitForElementVisible('div[aria-hidden = "false"] form div.item', 10);
-            $filtersCount = $C->getElementsCountByCssSelector('div[aria-hidden = "false"] form div.item');
-            $randomFilterNumber = rand(1, $filtersCount);
-            $C->waitForElementClickable("div[aria-hidden = false] form div.item:nth-of-type($randomFilterNumber) a", 10);
-            $C->executeJS("document.querySelector('div[aria-hidden = false] form div.item:nth-of-type($randomFilterNumber) a').click()");
-        } else {
-            $C->waitForElementVisible('div[aria-hidden = "false"] li.item', 10);
-            $filtersCount = $C->getElementsCountByCssSelector('div[aria-hidden = "false"] li.item');
-            $randomFilterNumber = rand(1, $filtersCount);
-            $C->waitForElementClickable("div[aria-hidden = false] li.item:nth-of-type($randomFilterNumber)", 10);
-            $C->executeJS("document.querySelector('div[aria-hidden = false] li.item:nth-of-type($randomFilterNumber)').click()");
+        $C->waitForElementVisible('div.is-widget-container-brand label', 10);
+        $filtersCount = $C->getElementsCountByCssSelector('div.is-widget-container-brand label');
+        $randomFilterNumber = rand(1, $filtersCount);
+        $C->waitForElementClickable("div.is-widget-container-brand .ais-RefinementList-item:nth-of-type($randomFilterNumber) label", 10);
+        $C->clickOnElementByCssSelector("div.is-widget-container-brand .ais-RefinementList-item:nth-of-type($randomFilterNumber) label");
+        $C->waitAjaxLoad();
+        $C->waitForElementVisible("//button[@class='ais-ClearRefinements-button action primary']", 10);
+        $value = $C->grabAttributeFrom("//label[@class='ais-RefinementList-label checked']/input", 'value');
+        $valueNew = $C->grabAttributeFrom("//li[@class='ais-Hits-item item product product-item'][1]//div[@class ='result-sub-content']/img", 'title');
+        if($value != $valueNew){
+            throw new Exception("Cart qty doesn't change $value !== $valueNew");
         }
-        $C->waitAjaxLoad();
-        $C->waitForElementVisible("a.action.clear.filter-clear", 10);
     }
 
     /**
@@ -140,9 +87,9 @@ class CategoryTester extends GlobalTester
     {
         $C = $this;
         $C->executeJS('window.scrollTo(0,0);');
-        $C->waitForElementClickable("a.action.clear.filter-clear", 10);
-        $C->click("a.action.clear.filter-clear");
-        $C->waitForElementNotVisible("a.action.clear.filter-clear", 10);
+        $C->waitForElementClickable("//button[@class='ais-ClearRefinements-button action primary']", 10);
+        $C->click("//button[@class='ais-ClearRefinements-button action primary']");
+        $C->waitForElementNotVisible("//button[@class='ais-ClearRefinements-button action primary']", 10);
     }
 
 }
--- tests/acceptance/CategoryCest.php
+++ tests/acceptance/CategoryCest.php
@@ -7,42 +7,14 @@ class CategoryCest
      * @param CategoryTester $C
      * @throws Exception
      */
-    public function filters(CategoryTester $C)
+    public function filtersAndSort(CategoryTester $C)
     {
         $C->amOnPage('/');
-        $C->waitPageLoad(10);
-        $C->openRandomNotEmptyCLP();
-        $C->selectRandomFilter();
-        $C->clearFilter();
-        $C->openRandomNotEmptyPLP();
-        $C->selectRandomFilter();
-        $C->clearFilter();
-        $C->openRandomNotEmptyBrandCategory();
-        $C->selectRandomFilter();
-        $C->clearFilter();
-    }
-
-    /**
-     * @param CategoryTester $C
-     * @throws Exception
-     */
-    public function sortByOnPLP(CategoryTester $C)
-    {
-        $C->amOnPage('/');
+        $C->closePopup();
         $C->waitPageLoad(10);
         $C->openRandomNotEmptyPLP();
+        $C->selectRandomFilter();
         $C->sortBySelect();
-    }
-
-    /**
-     * @param CategoryTester $C
-     * @throws Exception
-     */
-    public function sortByOnBrand(CategoryTester $C)
-    {
-        $C->amOnPage('/');
-        $C->waitPageLoad(10);
-        $C->openRandomNotEmptyBrandCategory();
-        $C->sortBySelect();
+        $C->clearFilter();
     }
 }
